9edb77f93b73493aa27d8b635c6766bbb03f4b63
Boxxy <Boxxy@DESKTOP-833NIST>
2021-07-14 20:51:03 +0800

    Add project files.

---
+++ UserAuth/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace UserAuth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.UsernameText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PasswordText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UsernameText
+            // 
+            this.UsernameText.ImeMode = System.Windows.Forms.ImeMode.HangulFull;
+            this.UsernameText.Location = new System.Drawing.Point(13, 24);
+            this.UsernameText.Name = "UsernameText";
+            this.UsernameText.Size = new System.Drawing.Size(157, 20);
+            this.UsernameText.TabIndex = 1;
+            this.UsernameText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // PasswordText
+            // 
+            this.PasswordText.Location = new System.Drawing.Point(12, 63);
+            this.PasswordText.Name = "PasswordText";
+            this.PasswordText.PasswordChar = '*';
+            this.PasswordText.Size = new System.Drawing.Size(157, 20);
+            this.PasswordText.TabIndex = 3;
+            this.PasswordText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.PasswordText.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(174, 125);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PasswordText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UsernameText);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.7D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UserAuth";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox UsernameText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PasswordText;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ UserAuth/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace UserAuth
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ("boxxy:123" == UsernameText.Text + ":" + PasswordText.Text)
+            {
+                MessageBox.Show("Signed in as " + UsernameText.Text, UsernameText.Text);
+            }
+            if ("default:password" == UsernameText.Text + ":" + PasswordText.Text)
+            {
+                MessageBox.Show("Checking Account " + UsernameText.Text, UsernameText.Text);
+                Thread.Sleep(5000);
+                System.Diagnostics.Process.Start("https://youtu.be/dQw4w9WgXcQ?t=17");
+                Thread.Sleep(2000);
+                MessageBox.Show("You've Just been rick rolled", UsernameText.Text);
+            }
+            else
+            {
+                MessageBox.Show("Username and Password is Incorrect");
+                MessageBox.Show("Make sure capitalization is correct");
+            }    
+            }
+        }
+}
